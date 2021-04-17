7f35fe53462a88e572b1e6922751f17082c8e7fc
Nafiz Mahmud <33199186+NafizMahmud@users.noreply.github.com>
2021-04-17 15:08:01 +0900

    JS

--- Javascript/Employee_Importance.js
+++ Javascript/Employee_Importance.js
@@ -1,10 +1,44 @@
-var data_employees =[[1, 5, [2, 3]], [2, 3, []], [3, 3, []]];
-var data_id =1;
-GetImportance(data_employees,data_id);
+/*
+[[1, 5, [2, 3]], [2, 3, [4]], [3, 3, []], [4, 3, []]], 1
 
-function GetImportance(employees, id) {
+    public int id; 1,
+    public int importance; 5,
+    public List<Integer> subordinates; [2, 3]
+
+*/
 
 
-console.log(employees[0][2]);
-console.log(id);
+/**
+ * @param {Employee[]} employees
+ * @param {number} id
+ * @return {number}
+ */
+
+var employees_data = [[1, 5, [2, 3]], [2, 3, [4]], [3, 3, []], [4, 3, []]];
+var id_data = 1;
+
+
+var GetImportance = function (employees, id) {
+    let emp = new Map();
+    for (i = 0; i < employees.length; i++) {
+        emp[employees_data[i][0]] = employees_data[i][1];
+        if(id === employees_data[i][0]) {target = employees_data[i];}
+
+    }
+    console.log(target);
+    GetImportanceHelper(target, 0);
+
 };
+
+function GetImportanceHelper(employee, importance) {
+
+    importance = importance + employee.importance;
+
+    if (employee.subordinates) {
+
+    }
+    //  [2, 3, [4]] = objedtt
+}
+
+
+GetImportance(employees_data, id_data);
